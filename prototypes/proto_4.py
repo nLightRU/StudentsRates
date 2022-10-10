00be7f1b4ae73edee63ce01925f67cd4252cb9d8
Ilya <northenlight6899@gmail.com>
2022-10-11 01:45:01 +0300

    some results

--- prototypes/proto_4.py
+++ prototypes/proto_4.py
@@ -27,7 +27,6 @@ def get_studies(number=10) -> list:
     return out
 
 def count_student_rate(id_student: int) -> int:
-
     exam_results = {'неуд': 0, 'уд': 3, 'хор':4, 'отл':5}
     test_results = {'Незачёт': 0, 'Зачёт': 3}
 
@@ -49,6 +48,44 @@ def count_student_rate(id_student: int) -> int:
     rate = (pts / pts_total) * 100
     print(student_name, 'rate:', round(rate,2))
 
+def get_student_results(id_student: int) -> dict:
+    student = db.session.query(db.StudentsTable).filter(db.StudentsTable.id == id_student).first()
+    id_study = student.id_study
+    semester = student.semester
+
+    # Get subjects
+    q = db.session.query(db.SubjectsTable)
+    rows = q.filter(db.SubjectsTable.id_study == id_study, 
+                    db.SubjectsTable.semester <= semester)
+    subjects = rows.order_by(db.SubjectsTable.semester).all()
+    
+    student_results = dict()
+
+    print(student.name, student.semester, 'семестр')
+    # Get results
+    for subject in subjects:
+        q = db.session.query(db.ResultsTable).filter(db.ResultsTable.id_subject == subject.id)
+        result = q.first().result
+        print(subject.name, subject.semester, 'семестр', '|', 'оценка', result)
+
+    return student_results
+
+
+def subject_result(id_subject: int):
+    subject = db.session.query(db.SubjectsTable).filter(db.SubjectsTable.id == id_subject).first()
+    id_study = subject.id_study
+    subject_semester = subject.semester
+
+    print(subject.name, subject.semester, 'семестр')
+    q = db.session.query(db.StudentsTable)
+    rows = q.filter(db.StudentsTable.id_study == id_study, 
+                        db.StudentsTable.semester == subject_semester)
+    students = rows.all()
+    for student in students:
+        result_obj = db.session.query(db.ResultsTable).filter(db.ResultsTable.id_student == student.id).first()
+        result = result_obj.result
+        print(student.name, result)
+
 
 ######### CLI INTERFACE #########
 
@@ -63,6 +100,16 @@ def count_student_rate(id_student: int) -> int:
 
 # count rate by 
 
-count_student_rate(1171)
+# TEST 
+# Programming with C
+# id = 6254, id_study = 39, semester = 3
+
+# TEST
+# Student 1163, id_study 39, semester 3
+# Student 1164, id_study 39, semester 7
+
+# count_student_rate(1164)
+get_student_results(1163)
+# subject_result(1130)
 
 
